3d55f27ee6129a0ab31e2eee8be30cd8d59ec4e4
gregfullman <greg.fullman@gmail.com>
2015-06-05 15:03:16 -0400

    Correcting Guid and VSGenero assembly name.

--- VSGenero/Properties/AssemblyInfo.cs
+++ VSGenero/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VSGenero2013")]
+[assembly: AssemblyTitle("VSGenero")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Greg Fullman")]
-[assembly: AssemblyProduct("VSGenero2013")]
+[assembly: AssemblyProduct("VSGenero")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]   
